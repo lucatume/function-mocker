b1fd70b34eae279235987c427b41e530847594e9
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-11-05 09:56:47 +0100

    removed specs to move to PHPUnit tests

--- spec/tad/FunctionMocker/FunctionMockerSpec.php
+++ spec/tad/FunctionMocker/FunctionMockerSpec.php
@@ -18,10 +18,4 @@
 			$this->shouldThrow( '\InvalidArgumentException' )->duringMock( 23 );
 		}
 
-		/**
-		 * it should return a mock object after construction
-		 */
-		public function it_should_return_a_mock_object_after_construction() {
-			$this::mock( 'undefined_function_1', 'foo' )->shouldHaveType( 'tad\FunctionMocker\Matcher' );
-		}
 	}
