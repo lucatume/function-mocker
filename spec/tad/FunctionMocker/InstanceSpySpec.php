f1e86e4fdcbb5a74f1a3c90de8c3931b55a13169
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-11-06 19:15:19 +0100

    fixed wrong dependency parameters

--- spec/tad/FunctionMocker/InstanceSpySpec.php
+++ spec/tad/FunctionMocker/InstanceSpySpec.php
@@ -14,14 +14,14 @@
 		/**
 		 * it can be built from a PHPUnit invocation and a mock object
 		 */
-		public function it_can_be_built_from_a_php_unit_invocation_and_a_mock_object( \PHPUnit_Framework_MockObject_Invocation $invocation, SomeClass $object ) {
+		public function it_can_be_built_from_a_php_unit_invocation_and_a_mock_object( \PHPUnit_Framework_MockObject_Matcher_Invocation $invocation, SomeClass $object ) {
 			$this::from( $invocation, $object )->shouldHaveType( 'tad\FunctionMocker\InstanceSpy' );
 		}
 
 		/**
 		 * it will forward non spy calls to the object
 		 */
-		public function it_will_forward_non_spy_calls_to_the_object( \PHPUnit_Framework_MockObject_Invocation $invocation, SomeClass $object ) {
+		public function it_will_forward_non_spy_calls_to_the_object( \PHPUnit_Framework_MockObject_Matcher_Invocation $invocation, SomeClass $object ) {
 			$object->instanceMethod()->willReturn( 123 );
 			$sut = $this::from( $invocation, $object );
 
@@ -31,8 +31,8 @@
 		/**
 		 * it will forward wasCalledTimes to the invocation object
 		 */
-		public function it_will_forward_was_called_times_to_the_invocation_object(\PHPUnit_Framework_MockObject_Invocation $invocation, SomeClass $object) {
+		public function it_will_forward_was_called_times_to_the_invocation_object( \PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount $invocation, SomeClass $object ) {
 			$invocations = array();
-			$invocation->getInvocations()->willReturn($invocations);
-		        }
+			$invocation->getInvocations()->willReturn( $invocations );
+		}
 	}
