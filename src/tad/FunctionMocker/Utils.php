92e36cef6df5239b92b7527887846dc02ba11306
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-12-24 18:57:22 +0100

    added default blacklist for more granular blacklisting control

--- src/tad/FunctionMocker/Utils.php
+++ src/tad/FunctionMocker/Utils.php
@@ -24,5 +24,22 @@
 
 			return $dir;
 		}
+
+		public static function filterPathListFrom( array $list, $rootDir ) {
+			\Arg::_( $rootDir, 'Root dir' )->assert( is_dir( $rootDir ), 'Root dir must be an existing dir' );
+
+			$_list = array_map( function ( $frag ) use ( $rootDir ) {
+				$path = $rootDir . DIRECTORY_SEPARATOR . self::normalizePathFrag( $frag );
+
+				return file_exists( $path ) ? $path : null;
+			}, $list );
+
+			return array_filter( $_list );
+		}
+
+		public static function getPatchworkFilePath(){
+			$rootDir = Utils::findParentContainingFrom( 'vendor', dirname( __FILE__ ) );
+			 return $rootDir . "/vendor/antecedent/patchwork/Patchwork.php";
+		}
 	}
 
