13f4fd782c1635e2bc0e1fd6321b9a3c1b420e07
Luca Tumedei <luca@theaveragedev.com>
2015-03-28 21:31:19 +0100

    added test for function replacement

--- src/tad/FunctionMocker/Call/Logger/SpyCallLogger.php
+++ src/tad/FunctionMocker/Call/Logger/SpyCallLogger.php
@@ -1,26 +1,73 @@
 <?php
 
-	namespace tad\FunctionMocker\Call\Logger;
+namespace tad\FunctionMocker\Call\Logger;
 
 
-	use tad\FunctionMocker\CallTrace;
+use tad\FunctionMocker\CallTrace;
 
-	class SpyCallLogger implements LoggerInterface {
+class SpyCallLogger implements LoggerInterface
+{
 
-		protected $calls = array();
+    protected $calls = array();
 
-		public function called( array $args = null ) {
-			$this->calls[] = CallTrace::fromArguments( $args );
-		}
+    public function called(array $args = null)
+    {
+        $this->calls[] = CallTrace::fromArguments($args);
+    }
 
-		public function getCallTimes( array $args = null ) {
-			$calls = $this->calls;
-			if ( $args ) {
-				$calls = array_filter( $calls, function ( CallTrace $call ) use ( $args ) {
-					return $call->getArguments() === $args;
-				} );
-			}
+    public function getCallTimes(array $args = null)
+    {
+        $calls = $this->calls;
+        if ($args) {
+            $calls = $this->getCallsMatchingArgs($args, $calls);
+        }
 
-			return count( $calls );
-		}
-	}
+        return count($calls);
+    }
+
+    /**
+     * @param $arg
+     * @param $callArg
+     * @return bool
+     */
+    private function compareArg($arg, $callArg)
+    {
+        return is_a($arg, '\PHPUnit_Framework_Constraint') ? $arg->evaluate($callArg, '', true) : $arg === $callArg;
+    }
+
+    /**
+     * @param $args
+     * @param $callArgs
+     * @return bool
+     */
+    private function compareArgs(array $args, array $callArgs)
+    {
+        if (count($args) > count($callArgs)) {
+            return false;
+        }
+        for ($i = 0; $i < count($args); $i++) {
+            $arg = $args[$i];
+            $callArg = $callArgs[$i];
+            if (!$this->compareArg($arg, $callArg)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array $args
+     * @param $calls
+     * @return array
+     */
+    private function getCallsMatchingArgs(array $args, $calls)
+    {
+        $calls = array_filter($calls, function (CallTrace $call) use ($args) {
+            $callArgs = $call->getArguments();
+
+            return $this->compareArgs($args, $callArgs);
+        });
+        return $calls;
+    }
+}
