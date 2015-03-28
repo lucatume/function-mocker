830192c3dadd0dd9a4f3118d3914ac08b9eb4a75
Luca Tumedei <luca@theaveragedev.com>
2015-03-28 21:40:55 +0100

    added test for function replacement

---
+++ tests/tad/FunctionMocker/FunctionMockerStaticMethodConstraintsCheckTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Luca
+ * Date: 28/03/15
+ * Time: 21:31
+ */
+
+namespace tests\tad\FunctionMocker;
+
+
+class FunctionMockerStaticMethodConstraintsCheckTest {
+
+}
