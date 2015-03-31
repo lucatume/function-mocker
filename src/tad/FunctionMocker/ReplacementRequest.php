b8a55ae7f2490a90f46ce95a11b4def0e4a55c48
Luca Tumedei <luca@theaveragedev.com>
2015-03-31 10:07:08 +0200

    refactored and tested the ReplacementRequest class

--- src/tad/FunctionMocker/ReplacementRequest.php
+++ src/tad/FunctionMocker/ReplacementRequest.php
@@ -40,24 +40,41 @@ class ReplacementRequest
         \Arg::_($mockRequest, 'Function or method name')->is_string();
         $instance = new self;
 
-        $instance->isMethod = preg_match("/^[\\w\\\\_]*::[\\w\\d_]+/um", $mockRequest) ? true : false;
+        $instance->isMethod = preg_match("/^[\\w\\\\_]*(::|->)[\\w\\d_]+/um", $mockRequest) ? true : false;
         $instance->isFunction = !$instance->isMethod;
         if ($instance->isFunction) {
             $instance->isStaticMethod = false;
             $instance->isInstanceMethod = false;
             $instance->requestClassName = '';
+            $instance->methodName = $mockRequest;
         } else {
             $request = preg_split('/(::|->)/', $mockRequest);
             $className = $request[0];
             $methodName = $request[1];
             $reflection = new \ReflectionMethod($className, $methodName);
             $instance->isInstanceMethod = !$reflection->isStatic();
+
+            $instance->ensure_matching_symbol($mockRequest);
+
             $instance->isStaticMethod = $reflection->isStatic();
             $instance->requestClassName = $reflection->class;
             $instance->methodName = $reflection->name;
         }
 
         return $instance;
+    }
+
+    /**
+     * @param $requestString
+     */
+    private function ensure_matching_symbol($requestString)
+    {
+        $m = [];
+        preg_match('/(::|->)/', $requestString, $m);
+        $symbol = $m[1];
+        if ($symbol === '->' && !$this->isInstanceMethod()) {
+            throw new \InvalidArgumentException('Request was for a static method but the \'->\' symbol was used; keep it clear.');
+        }
     }
 
     public function isFunction()
