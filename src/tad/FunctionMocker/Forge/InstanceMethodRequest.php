9858c08079f425b51a738bcc021f02cac0d643ec
Luca Tumedei <luca@theaveragedev.com>
2015-04-09 20:20:58 +0200

    added check for interface and trait class

--- src/tad/FunctionMocker/Forge/InstanceMethodRequest.php
+++ src/tad/FunctionMocker/Forge/InstanceMethodRequest.php
@@ -11,17 +11,20 @@ namespace tad\FunctionMocker\Forge;
 
 use tad\FunctionMocker\ReplacementRequest;
 
-class InstanceMethodRequest extends ReplacementRequest {
-	public static function instance( $class ) {
-		\Arg::_( $class, 'Class name' )->is_string()->assert( class_exists( $class ), 'Class must be a defined one' );
-		$instance                   = new self;
-		$instance->requestClassName = $class;
-		$instance->isStaticMethod   = false;
-		$instance->isInstanceMethod = true;
-		$instance->isFunction       = false;
-		$instance->isMethod         = true;
-		$instance->methodName       = '';
+class InstanceMethodRequest extends ReplacementRequest
+{
 
-		return $instance;
-	}
+    public static function instance($class)
+    {
+        \Arg::_($class, 'Class name')->is_string()->assert(class_exists($class) || interface_exists($class) || trait_exists($class), 'Class must be a defined one');
+        $instance = new self;
+        $instance->requestClassName = $class;
+        $instance->isStaticMethod = false;
+        $instance->isInstanceMethod = true;
+        $instance->isFunction = false;
+        $instance->isMethod = true;
+        $instance->methodName = '';
+
+        return $instance;
+    }
 }
