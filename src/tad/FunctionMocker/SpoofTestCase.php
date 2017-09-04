77a240d4eae76bd4a448b9f8cc04aea819615108
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-09-04 11:39:40 +0200

    extend legacy PHPUnit classes if available

--- src/tad/FunctionMocker/SpoofTestCase.php
+++ src/tad/FunctionMocker/SpoofTestCase.php
@@ -2,16 +2,13 @@
 
 namespace tad\FunctionMocker;
 
+if (class_exists('PHPUnit_Framework_TestCase')) {
+	// PHPUnit < 6.0 support
+	class SpoofTestCase extends \PHPUnit_Framework_TestCase {
 
-if (class_exists('\\PHPunit\\Framework\\TestCase')) {
-    class SpoofTestCase extends \PHPunit\Framework\TestCase
-    {
-
-    }
+	}
 } else {
-    // PHPUnit < 6.0 support
-    class SpoofTestCase extends \PHPUnit_Framework_TestCase
-    {
+	class SpoofTestCase extends \PHPunit\Framework\TestCase {
 
-    }
+	}
 }
