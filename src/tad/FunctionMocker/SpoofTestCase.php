d319da21b01b1c0820458975c10eb5c0debcf474
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-09-04 11:45:00 +0200

    tighter check on PHPUnit test case class

--- src/tad/FunctionMocker/SpoofTestCase.php
+++ src/tad/FunctionMocker/SpoofTestCase.php
@@ -2,7 +2,7 @@
 
 namespace tad\FunctionMocker;
 
-if (class_exists('PHPUnit_Framework_TestCase')) {
+if (class_exists('PHPUnit_Framework_TestCase') && !class_exists('\\PHPunit\\Framework\\TestCase')) {
 	// PHPUnit < 6.0 support
 	class SpoofTestCase extends \PHPUnit_Framework_TestCase {
 
