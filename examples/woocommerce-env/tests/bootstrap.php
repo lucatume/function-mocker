5d180342b205f5dfe10f347da9f85e68eebcad71
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-07-27 09:51:40 +0200

    update option names to normalized ones

--- examples/woocommerce-env/tests/bootstrap.php
+++ examples/woocommerce-env/tests/bootstrap.php
@@ -4,6 +4,13 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 // init Function Mocker
 \tad\FunctionMocker\FunctionMocker::init( [
 	'cache-path'            => __DIR__ . '/../../../../_cache/fm-woocommerce-env-example',
-	'whitelist'             => [ __DIR__, dirname( __DIR__ ) . '/src' ],
+	'whitelist'             => [ 
+		__DIR__,
+		 dirname( __DIR__ ) . '/src',
+		__DIR__ . '/envs/woocommerce'
+	],
+	'envs' => [ 
+		'WordPress',
+		__DIR__ . '/envs/woocommerce/bootstrap.php',
+	],
 ] );
-
