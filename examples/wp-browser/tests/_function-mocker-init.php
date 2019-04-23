83a01817b68edc2e0564c114a4e0d449bba9facc
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-04-23 22:51:13 +0200

    test(wp-browser example): fix bootstrap files

--- examples/wp-browser/tests/_function-mocker-init.php
+++ examples/wp-browser/tests/_function-mocker-init.php
@@ -10,4 +10,5 @@ require_once( __DIR__ . '/../vendor/autoload.php' );
 		dirname( __DIR__ ) . '/vendor/wordpress/src',
 	],
 	'redefinable-internals' => [ 'time' ],
+	'load-wp-env' => false
 ] );
--- examples/wp-browser/tests/integration/_bootstrap.php
+++ examples/wp-browser/tests/integration/_bootstrap.php
@@ -1,7 +1,1 @@
 <?php
-// init Function Mocker
-\tad\FunctionMocker\FunctionMocker::init( [
-	'whitelist'             => [ __DIR__, dirname( __DIR__, 2 ) . '/src' ],
-	'redefinable-internals' => [ 'time' ],
-	'load-wp-env' => false
-] );
